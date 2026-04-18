7300fa2b1a29703b20793e379a969bc1107046dc
topyohsien <topyohsien@gmail.com>
2026-04-18 21:42:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const resultDiv = document.getElementById('result');
 // 這些資訊填入你剛才在 ACRCloud 申請到的 Key
 const config = {
     host: '你的_HOST',
-    access_key: '你的_ACCESS_KEY',
-    access_secret: '你的_ACCESS_SECRET',
+    access_key: '9ef404c2c6119a39f4228c27d747bddb',
+    access_secret: 'mSz4da3dhnib6Nxg0b3yC10tL6Ch9jtqEIMfNaU0',
     type: 'audio'
 };
 
